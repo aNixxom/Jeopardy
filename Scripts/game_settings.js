c041c093283cba48f4a6fb5ec7b1848af7849459
aNixxom <91269723+aNixxom@users.noreply.github.com>
2022-03-25 22:43:44 -0400

    Update game_settings.js

--- Scripts/game_settings.js
+++ Scripts/game_settings.js
@@ -6,6 +6,12 @@ var isOpen = false
 let mainBody = document.querySelector('body')
 let player_headers = document.getElementsByClassName('headers'), i;
 
+let frfc = document.getElementById("frfc")
+let secondrsecondc = document.getElementById("2r2c")
+let threerthreec = document.getElementById("3r3c")
+let four4fourc = document.getElementById("4r4c")
+let fiverfivec = document.getElementById("5r5c")
+
 function openMenu() {
     if(isOpen == false && _dom.viewingQuestion == false) {
         
@@ -32,12 +38,14 @@ _dom.doubleTimeSwitch.onclick = function toggle(event) {
     if(_dom.doubleTimeCheatEnabled == false && clicked == "settings-toggle-button-one") {
         _dom.doubleTimeSwitch.setAttribute("name", "radio-button-on-outline")
         _dom.doubleTimeCheatEnabled = true
+        document.querySelector('body').style.border = "yellow solid 2px"
         console.log("doubleTimeCheatEnabled: " + _dom.doubleTimeCheatEnabled)
         _dom.questionLength = 12000
     } else if(_dom.doubleTimeCheatEnabled == true && clicked == "settings-toggle-button-one" ) {
         _dom.doubleTimeSwitch.setAttribute("name", "radio-button-off-outline")
         _dom.doubleTimeCheatEnabled = false
         _dom.questionLength = 6000
+        mainBody.style.border = "2px transparent solid" 
         console.log("doubleTimeCheatEnabled: " + _dom.doubleTimeCheatEnabled)
     }
 }
@@ -58,6 +66,51 @@ _dom.editModeSwitch.onclick = function toggle(event) {
             player_headers[i].className = "headers"
         }
         mainBody.style.border = "2px transparent solid"  
+    }
+}
+
+_dom.doublePointsSwitch.onclick = function toggle(event) {
+    var clicked = event.target.classList[0]
+    if(_dom.doublePointToggled == false && clicked == "settings-toggle-button-three") {
+        _dom.doublePointsSwitch.setAttribute("name", "radio-button-on-outline")
+        document.querySelector('body').style.border = "green solid 2px"
+        for(i = 0; i < frfc.children.length; i++) {
+            frfc.children[i].innerHTML = "$400"
+        }
+        for(i = 0; i < secondrsecondc.children.length; i++) {
+            secondrsecondc.children[i].innerHTML = "$800"
+        }
+        for(i = 0; i < threerthreec.children.length; i++) {
+            threerthreec.children[i].innerHTML = "$1200"
+        }
+        for(i= 0; i < four4fourc.children.length; i++) {
+            four4fourc.children[i].innerHTML = "$1600"
+        }
+        for(i = 0; i < fiverfivec.children.length; i++) {
+            fiverfivec.children[i].innerHTML = "$2000"
+        }
+        _dom.default_point_value = 400
+        _dom.doublePointToggled = true
+    } else if(_dom.doublePointToggled == true) {
+        _dom.doublePointsSwitch.setAttribute("name", "radio-button-off-outline")
+        mainBody.style.border = "2px transparent solid"  
+        for(i = 0; i < frfc.children.length; i++) {
+            frfc.children[i].innerHTML = "$200"
+        }
+        for(i = 0; i < secondrsecondc.children.length; i++) {
+            secondrsecondc.children[i].innerHTML = "$400"
+        }
+        for(i = 0; i < threerthreec.children.length; i++) {
+            threerthreec.children[i].innerHTML = "$600"
+        }
+        for(i= 0; i < four4fourc.children.length; i++) {
+            four4fourc.children[i].innerHTML = "$800"
+        }
+        for(i = 0; i < fiverfivec.children.length; i++) {
+            fiverfivec.children[i].innerHTML = "$1000"
+        }
+        _dom.default_point_value = 200
+        _dom.doublePointToggled = false
     }
 }
 
