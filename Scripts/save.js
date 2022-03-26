c845ce11f51a7fb8bec7edb73143a359a2eebcb0
aNixxom <91269723+aNixxom@users.noreply.github.com>
2022-03-25 22:44:56 -0400

    Update save.js

--- Scripts/save.js
+++ Scripts/save.js
@@ -4,10 +4,14 @@ let deleteButton = document.getElementById('delete')
 let popup = document.getElementById('popup')
 
 import { closeMenu } from '/Scripts/game_settings.js'
+import { systemMessage, screenShake} from '/Scripts/createNewPlayers.js'
+import {_dom} from '/Scripts/game_variables.js'
 
 saveButton.addEventListener('click', saveGame)
 loadButton.addEventListener('click', loadSave)
 deleteButton.addEventListener('click', deleteLocalStorage)
+
+
 
 function saveGame() {
 
@@ -19,30 +23,44 @@ function saveGame() {
     localStorage.setItem('player2_score', document.getElementById('player2_score').innerText)
     localStorage.setItem('player3_score', document.getElementById('player3_score').innerText)
 
-    popup.innerHTML = "Game saved"
-    popup.className = "show"
-    setTimeout(function(){popup.className = popup.className.replace("show", "") }, 3000)
     
-
+    systemMessage("Game saved")
+    
 }
 
 function loadSave() {
+    if(localStorage.length === 0) {
+       systemMessage("No game data found")
+       screenShake()
+       return
+    }
     document.getElementById('player1_name').innerText = localStorage.getItem('player1_name')
     document.getElementById('player2_name').innerText = localStorage.getItem('player2_name')
-    document.getElementById('player2_name').innerText = localStorage.getItem('player2_name')
+    document.getElementById('player3_name').innerText = localStorage.getItem('player3_name')
 
-    document.getElementById('player1_score').innerText = localStorage.getItem('player1_score')
-    document.getElementById('player2_score').innerText = localStorage.getItem('player2_score')
-    document.getElementById('player3_score').innerText = localStorage.getItem('player3_score')
+    document.getElementById('player1_score').innerText = parseInt(localStorage.getItem('player1_score'))
+    document.getElementById('player2_score').innerText = parseInt(localStorage.getItem('player2_score'))
+    document.getElementById('player3_score').innerText = parseInt(localStorage.getItem('player3_score'))
 
-    popup.innerHTML = "Loaded Game"
-    popup.className = "show"
-    setTimeout(function(){popup.className = popup.className.replace("show", "") }, 3000)
+    _dom.p1_score = localStorage.getItem('player1_score')
+    _dom.p2_score = localStorage.getItem('player2_score')
+    _dom.p3_score = localStorage.getItem('player3_score')
+
+    _dom.loadedGame = true
+    console.log(_dom.loadedGame)
+
+    systemMessage("Loaded Save")
     closeMenu()
 }
 
 function deleteLocalStorage() {
+    if(localStorage.length === 0) {
+        systemMessage("No game data found")
+        screenShake()
+        return
+     }
     confirm("Delete saved game?")
     localStorage.clear()
+    systemMessage("Deleted saved games")
 }
 
