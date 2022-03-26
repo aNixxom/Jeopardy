e69d600d3e6d75039eadde1a114b24c2ed95c274
aNixxom <91269723+aNixxom@users.noreply.github.com>
2022-03-25 22:41:12 -0400

    Updated createNewPlayer.js

--- Scripts/createNewPlayers.js
+++ Scripts/createNewPlayers.js
@@ -1,9 +1,10 @@
 import {_dom} from '/Scripts/game_variables.js'
 import {closeMenu} from '/Scripts/game_settings.js'
 import {_pVars} from '/Scripts/test.js'
-import { newPlayerNames } from './edit_player_name.js'
+
 
 var current_player_count = document.getElementById('player_count')
+let settingsContainer = document.getElementById('settingsContainer')
 let popup = document.getElementById('popup')
 let player_list = [ // Default player count
     'player1_score',
@@ -23,31 +24,41 @@ let player_buttons_list = [
     'player3_buttons'
 ]
 
-// console.log(player_list) //Debug
-// console.log(player_name_list) //Debug
-// console.log(player_buttons_list) //Debug
+function systemMessage(message) {
+    popup.innerHTML = message
+    popup.className = "show"
+    setTimeout(function(){popup.className = popup.className.replace("show", "") }, 3000)
+}
+
+function screenShake() {
+    settingsContainer.className = "shake shake-horizontal"
+    setTimeout(function(){settingsContainer.className = settingsContainer.className.replace("shake shake-horizontal", "") }, 110)
+}
 
 function addPlayer() {
     if(_pVars.total_players >= 8) {
-        alert("You can not have more than 7 players")
+        screenShake()
+        systemMessage("You can't have more than 7 players")
         return
     } else {
-        _pVars.players_added += 1
     }
     
-    _pVars.total_players = _pVars.players_added + _pVars.starting_players 
-    var player_Number = _pVars.total_players.toString()
-    var number_of_players = (` ${player_Number} `)
-
     const tr_player_names = document.getElementById('player_names')
     let th_player_name = document.createElement('th') // Creats player name
     let playerName = prompt("Enter Name")
-    newPlayerNames.push(playerName)
-    console.log(newPlayerNames)
 
     if(playerName == null || playerName == "") { // If player does not enter a name, or they hit cancel return and break the function
+        screenShake()
+        systemMessage('Please enter a valid name')
         return
-    } else {
+    } else if (playerName != null && playerName != "") {
+
+        _pVars.players_added += 1 // add 1 to player list
+        _pVars.total_players = _pVars.players_added + _pVars.starting_players // new player total 
+        var player_Number = _pVars.total_players.toString()
+        console.log(_pVars.total_players)
+        var number_of_players = (` ${player_Number} `)
+
         var s_test = ("player" + player_Number + "_score")
         var n_test = ("player" + player_Number + "_name")
         var b_test = ("player" + player_Number + "_buttons")
@@ -55,57 +66,53 @@ function addPlayer() {
         player_list.push(s_test)
         player_name_list.push(n_test)
 
-        // console.log(player_list) // Debug
-        // console.log(player_name_list) //Debug 
-        // console.log(player_buttons_list) // Debug
+        current_player_count.innerText = number_of_players
+
+        th_player_name.setAttribute('class', "headers")
+        th_player_name.setAttribute('id', `player${player_Number}_name`)
+        th_player_name.innerText = playerName // sets player name to the table cell
+        tr_player_names.appendChild(th_player_name) // Appends player name to the player name table
+
+        const tr_player_score_text = document.getElementById('player_score_text')
+        let th_player_score_text = document.createElement('th') // Creats player score text (0)
+
+
+        th_player_score_text.setAttribute('class', "score_boxes ")
+        th_player_score_text.setAttribute('id', "player" + player_Number + "_score")
+        th_player_score_text.innerText = 0
+        tr_player_score_text.appendChild(th_player_score_text)
+
+        const tr_player_score_buttons = document.getElementById('player_score_buttons')
+        let td_player_score_buttons = document.createElement('td')
+        let td_add_button = document.createElement('button') // Creates ADD button for player
+        let td_take_button = document.createElement('button') // Creats TAKE button for player
+
+        td_add_button.innerText = "ADD"
+        td_add_button.setAttribute('id', `add_p${player_Number}`)
+        td_add_button.setAttribute('class', "points-button")
+
+        td_take_button.innerText = "TAKE"
+        td_take_button.setAttribute('id', `take_p${player_Number}`)
+        td_take_button.setAttribute('class', "points-button")
+
+        td_player_score_buttons.setAttribute('id', `player${player_Number}_buttons`)
+
+        td_player_score_buttons.appendChild(td_add_button) // Appends ADD button to new player
+        td_player_score_buttons.appendChild(td_take_button) // Appends TAKE button to new player
+        tr_player_score_buttons.appendChild(td_player_score_buttons) // Appends TAKE & ADD to MAIN table
+
+        console.table(player_name_list)
+
+        closeMenu()
+        
     }
-    current_player_count.innerText = number_of_players
-
-    th_player_name.setAttribute('class', "headers")
-    th_player_name.setAttribute('id', `player${player_Number}_name`)
-    th_player_name.innerText = playerName // sets player name to the table cell
-    tr_player_names.appendChild(th_player_name) // Appends player name to the player name table
-
-    const tr_player_score_text = document.getElementById('player_score_text')
-    let th_player_score_text = document.createElement('th') // Creats player score text (0)
-    //let player_score_id = ("player" + player_Number + "_score") //Not needed
-
-
-    th_player_score_text.setAttribute('class', "score_boxes")
-    th_player_score_text.setAttribute('id', "player" + player_Number + "_score")
-    th_player_score_text.innerText = 0
-    tr_player_score_text.appendChild(th_player_score_text)
-
-    const tr_player_score_buttons = document.getElementById('player_score_buttons')
-    let td_player_score_buttons = document.createElement('td')
-    let td_add_button = document.createElement('button') // Creates ADD button for player
-    let td_take_button = document.createElement('button') // Creats TAKE button for player
-    //var add_player_id = ("add_p" + player_Number) // Should return a string Ex: "add_p5" (Not needed)
-    //var take_player_id = ("take_p" + player_Number) // Should return a string Ex: "take_p5" (Not needed)
-
-    td_add_button.innerText = "ADD"
-    td_add_button.setAttribute('id', `add_p${player_Number}`)
-
-    td_take_button.innerText = "TAKE"
-    td_take_button.setAttribute('id', `take_p${player_Number}`)
-
-    td_player_score_buttons.setAttribute('id', `player${player_Number}_buttons`)
-
-    td_player_score_buttons.appendChild(td_add_button) // Appends ADD button to new player
-    td_player_score_buttons.appendChild(td_take_button) // Appends TAKE button to new player
-    tr_player_score_buttons.appendChild(td_player_score_buttons) // Appends TAKE & ADD to MAIN table
-
-    closeMenu()
-
 }
 
 function takePlayer() {
 
     if(_pVars.total_players <= 3) {
-
-        popup.innerHTML = "You can't have less than 3 players"
-        popup.className = "show"
-        setTimeout(function(){popup.className = popup.className.replace("show", "") }, 3000)
+        systemMessage("You can't have less than 3 players");
+        screenShake()
         return
     }
 
@@ -115,15 +122,12 @@ function takePlayer() {
 
     var player_Number = _pVars.total_players.toString()
     var number_of_players = (` ${player_Number} `)
+
     current_player_count.innerText = number_of_players
-    //player_list.pop(number_of_players)
 
-    //console.log(player_list) //Debuging
-    //console.log(player_list[player_list.length - 1]) //Debug
-
-    let player_score_to_remove = player_list[player_list.length - 1] //gets last score in list
-    let player_name_to_remove = player_name_list[player_name_list.length -1] //gets last name in list
-    let player_buttons_to_remove = player_buttons_list[player_buttons_list.length -1] //gets last button in list
+    let player_score_to_remove = player_list.pop()//gets last score in list
+    let player_name_to_remove = player_name_list.pop() //gets last name in list
+    let player_buttons_to_remove = player_buttons_list.pop() //gets last button in list
 
     
 
@@ -153,8 +157,10 @@ function takePlayer() {
         alert("You can not have 0 players")
         return
     } 
+    console.table(player_name_list)
+
 }
 _dom.add_player.onclick = function () {addPlayer()}
 _dom.take_player.onclick = function () {takePlayer()}
 
-export { player_buttons_list, player_name_list, player_list}
+export { player_buttons_list, player_name_list, player_list, systemMessage, screenShake}
