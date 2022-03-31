3b5f74200bbdec921ee434d5a8f6deddaa15c507
aNixxom <jacob@jacobm.dev>
2022-03-31 10:43:45 -0400

    Updated game_buttons.js

--- Scripts/game_buttons.js
+++ Scripts/game_buttons.js
@@ -1,21 +1,24 @@
 import {_dom} from '/Scripts/game_variables.js'
 
-let p1s = document.getElementById("player1_score")
+
 let p2s = document.getElementById("player2_score")
 let p3s = document.getElementById("player3_score")
 
-
-
 window.addEventListener('click', function(event) {
   let clicked_button = event.target.id
+
+  
   
   if(clicked_button == 'add_p1') {
+    let p1s = document.getElementById('player1_score')
     _dom.p1_score = +_dom.default_point_value + +_dom.p1_score
-    p1s.innerHTML = _dom.p1_score
+    p1s.innerText= _dom.p1_score
   } else if (clicked_button == 'add_p2') {
+    let p2s = document.getElementById('player2_score')
     _dom.p2_score = +_dom.default_point_value + +_dom.p2_score
     p2s.innerText = _dom.p2_score
   } else if (clicked_button == 'add_p3') {
+    let p3s = document.getElementById('player3_score')
     _dom.p3_score = +_dom.default_point_value + +_dom.p3_score
     p3s.innerText = _dom.p3_score
   } else if (clicked_button == 'add_p4') {
@@ -39,12 +42,15 @@ window.addEventListener('click', function(event) {
   let clicked_button = event.target.id
   
   if(clicked_button == 'take_p1') {
+    let p1s = document.getElementById('player1_score')
     _dom.p1_score -= _dom.default_point_value
     p1s.innerText = _dom.p1_score
   } else if (clicked_button == 'take_p2') {
+    let p2s = document.getElementById('player2_score')
     _dom.p2_score -= _dom.default_point_value
     p2s.innerText = _dom.p2_score
   } else if (clicked_button == 'take_p3') {
+    let p3s = document.getElementById('player3_score')
     _dom.p3_score  -= _dom.default_point_value
     p3s.innerHTML = _dom.p3_score
   } else if (clicked_button == 'take_p4') {
