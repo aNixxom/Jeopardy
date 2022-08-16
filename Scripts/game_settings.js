d0c9d43873e915c20da36f234a57021ffa3bc76b
Jacob D. Martin <91269723+aNixxom@users.noreply.github.com>
2022-08-16 18:52:13 -0400

    Update game_settings.js

--- Scripts/game_settings.js
+++ Scripts/game_settings.js
@@ -2,7 +2,7 @@ import {_dom} from '/Scripts/game_variables.js'
 
 
 
-var isOpen = false
+let isOpen = false
 let mainBody = document.querySelector('body')
 let player_headers = document.getElementsByClassName('headers'), i;
 
@@ -14,9 +14,9 @@ let fiverfivec = document.getElementById("5r5c")
 
 function openMenu() {
     if(isOpen == false && _dom.viewingQuestion == false) {
-
+        
         _dom.main.style.display = "none"
-        // _dom.settings_menu.setAttribute('class', 'settings-menu animate__animated animate__zoomIn')
+        _dom.settings_menu.setAttribute('class', 'settings-menu animate__animated animate__zoomIn')
         _dom.settings_menu.style.display = "block"
         isOpen = true
     } else if(isOpen == true) {
@@ -34,7 +34,7 @@ _dom.menu_button.onclick = function () { openMenu() }
 
 
 _dom.doubleTimeSwitch.onclick = function toggle(event) {
-    var clicked = event.target.classList[0]
+    let clicked = event.target.classList[0]
     if(_dom.doubleTimeCheatEnabled == false && clicked == "settings-toggle-button-one") {
         _dom.doubleTimeSwitch.setAttribute("name", "radio-button-on-outline")
         _dom.doubleTimeCheatEnabled = true
@@ -45,13 +45,13 @@ _dom.doubleTimeSwitch.onclick = function toggle(event) {
         _dom.doubleTimeSwitch.setAttribute("name", "radio-button-off-outline")
         _dom.doubleTimeCheatEnabled = false
         _dom.questionLength = 6000
-        mainBody.style.border = "2px transparent solid"
+        mainBody.style.border = "2px transparent solid" 
         console.log("doubleTimeCheatEnabled: " + _dom.doubleTimeCheatEnabled)
     }
 }
 
 _dom.editModeSwitch.onclick = function toggle(event) {
-    var clicked = event.target.classList[0]
+    let clicked = event.target.classList[0]
     if(_dom.editModeToggled == false && clicked == "settings-toggle-button-two") {
         _dom.editModeSwitch.setAttribute("name", "radio-button-on-outline")
         for (i = 0; i < player_headers.length; i++) {
@@ -70,24 +70,24 @@ _dom.editModeSwitch.onclick = function toggle(event) {
 }
 
 _dom.doublePointsSwitch.onclick = function toggle(event) {
-    var clicked = event.target.classList[0]
+    let clicked = event.target.classList[0]
     if(_dom.doublePointToggled == false && clicked == "settings-toggle-button-three") {
         _dom.doublePointsSwitch.setAttribute("name", "radio-button-on-outline")
         document.querySelector('body').style.border = "green solid 2px"
         for(i = 0; i < frfc.children.length; i++) {
-            frfc.children[i].innerHTML = "$400"
+           checkQuestion(frfc, '$400')
         }
         for(i = 0; i < secondrsecondc.children.length; i++) {
-            secondrsecondc.children[i].innerHTML = "$800"
+            checkQuestion(secondrsecondc, '$800')
         }
         for(i = 0; i < threerthreec.children.length; i++) {
-            threerthreec.children[i].innerHTML = "$1200"
+            checkQuestion(threerthreec, '$1200')
         }
         for(i= 0; i < four4fourc.children.length; i++) {
-            four4fourc.children[i].innerHTML = "$1600"
+            checkQuestion(four4fourc, '$1600')
         }
         for(i = 0; i < fiverfivec.children.length; i++) {
-            fiverfivec.children[i].innerHTML = "$2000"
+            checkQuestion(fiverfivec, '$2000')
         }
         _dom.default_point_value = 400
         _dom.doublePointToggled = true
@@ -95,23 +95,31 @@ _dom.doublePointsSwitch.onclick = function toggle(event) {
         _dom.doublePointsSwitch.setAttribute("name", "radio-button-off-outline")
         mainBody.style.border = "2px transparent solid"  
         for(i = 0; i < frfc.children.length; i++) {
-            frfc.children[i].innerHTML = "$200"
+            checkQuestion(frfc, '$200')
         }
         for(i = 0; i < secondrsecondc.children.length; i++) {
-            secondrsecondc.children[i].innerHTML = "$400"
+            checkQuestion(secondrsecondc, '$400')
         }
         for(i = 0; i < threerthreec.children.length; i++) {
-            threerthreec.children[i].innerHTML = "$600"
+            checkQuestion(threerthreec, '$600')
         }
         for(i= 0; i < four4fourc.children.length; i++) {
-            four4fourc.children[i].innerHTML = "$800"
+            checkQuestion(four4fourc, '$800')
         }
         for(i = 0; i < fiverfivec.children.length; i++) {
-            fiverfivec.children[i].innerHTML = "$1000"
+            checkQuestion(fiverfivec, '$1000')
         }
         _dom.default_point_value = 200
         _dom.doublePointToggled = false
     }
 }
+
+
+function checkQuestion(question, points) {
+    if(question.children[i].innerHTML != '-') {
+        question.children[i].innerHTML = points
+    }
+}
+
 
 export {closeMenu}
