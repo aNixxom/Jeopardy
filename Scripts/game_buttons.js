23583dd8896e2e73c45e68ff747f5d2864229577
Jacob D. Martin <91269723+aNixxom@users.noreply.github.com>
2022-08-16 18:50:45 -0400

    Update game_buttons.js

--- Scripts/game_buttons.js
+++ Scripts/game_buttons.js
@@ -1,24 +1,19 @@
 import {_dom} from '/Scripts/game_variables.js'
 
-
+let p1s = document.getElementById("player1_score")
 let p2s = document.getElementById("player2_score")
 let p3s = document.getElementById("player3_score")
 
 window.addEventListener('click', function(event) {
   let clicked_button = event.target.id
-
-
-
+  
   if(clicked_button == 'add_p1') {
-    let p1s = document.getElementById('player1_score')
     _dom.p1_score = +_dom.default_point_value + +_dom.p1_score
-    p1s.innerText= _dom.p1_score
+    p1s.innerHTML = _dom.p1_score
   } else if (clicked_button == 'add_p2') {
-    let p2s = document.getElementById('player2_score')
     _dom.p2_score = +_dom.default_point_value + +_dom.p2_score
     p2s.innerText = _dom.p2_score
   } else if (clicked_button == 'add_p3') {
-    let p3s = document.getElementById('player3_score')
     _dom.p3_score = +_dom.default_point_value + +_dom.p3_score
     p3s.innerText = _dom.p3_score
   } else if (clicked_button == 'add_p4') {
@@ -33,7 +28,7 @@ window.addEventListener('click', function(event) {
   } else if (clicked_button == 'add_p7') {
     _dom.p7_score = +_dom.default_point_value + +_dom.p7_score
     document.getElementById('player7_score').innerText = _dom.p7_score
-  }
+  } 
 })
 
 
@@ -42,15 +37,12 @@ window.addEventListener('click', function(event) {
   let clicked_button = event.target.id
   
   if(clicked_button == 'take_p1') {
-    let p1s = document.getElementById('player1_score')
     _dom.p1_score -= _dom.default_point_value
     p1s.innerText = _dom.p1_score
   } else if (clicked_button == 'take_p2') {
-    let p2s = document.getElementById('player2_score')
     _dom.p2_score -= _dom.default_point_value
     p2s.innerText = _dom.p2_score
   } else if (clicked_button == 'take_p3') {
-    let p3s = document.getElementById('player3_score')
     _dom.p3_score  -= _dom.default_point_value
     p3s.innerHTML = _dom.p3_score
   } else if (clicked_button == 'take_p4') {
