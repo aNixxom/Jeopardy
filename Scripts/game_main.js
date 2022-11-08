22f52f5a9d85f3a4139690ebb9d41ae112b1644b
Jacob D. Martin <91269723+aNixxom@users.noreply.github.com>
2022-11-08 11:19:25 -0500

    Updated game_main.js
    I added a simple answer check. Added a sound when you click the right answer.

--- Scripts/game_main.js
+++ Scripts/game_main.js
@@ -1,4 +1,3 @@
-
 import {_dom} from '/Scripts/game_variables.js'
 
 let every_id = document.querySelectorAll('*[id]')
@@ -10,8 +9,11 @@ const stopAnimation = () => { // Progress Bar animation event (end)
         _dom.main.style.visibility = "visible"
         _dom.viewingQuestion = false
         stopMusic(_dom.countdown_music)
-        _dom.times_up.play()
-        
+        if(_dom.correct_answer) {
+            _dom.corret_answer_sound.play() 
+        } else if(_dom.correct_answer == false) {
+            _dom.times_up.play()
+        }
     }
 }
 
@@ -19,7 +21,7 @@ const playAnimation = ()  => { // Progress Bar animation event (start)
     const progressBar = document.querySelectorAll(".timer-bar") // Gets all the questions with ".timer-bar"
     for (var i = 0; i < progressBar.length;  i++) {
         if(_dom.doubleTimeCheatEnabled == true) {
-            background_music.play()
+            _dom.countdown_music.play()
             progressBar[i].setAttribute("id", 'play-timer-animation-double');
             setTimeout(stopMusic(_dom.countdown_music), 12000)
 
@@ -32,46 +34,58 @@ const playAnimation = ()  => { // Progress Bar animation event (start)
 }
 
 window.addEventListener('click', function(event){
-const clicked_element = document.getElementById(event.target.id)
+    const clicked_element = document.getElementById(event.target.id) // $
 
     for (let i = 0; i < every_id.length; i++) {
+        
         try {
             if(clicked_element.classList[1] == every_id[i].id) {
                 const question_id = clicked_element.classList[1]
                 const question = document.getElementById(question_id)
+                let choices = question.children[1]
+                 console.log(choices)
+                
 
-                // console.info("CURRENT QUESTION:", question_id)
-                // console.log(_dom.doubleTimeCheatEnabled)
+
                 _dom.viewingQuestion = true
+
+                question.setAttribute('class', 'questions animate__animated animate__zoomIn')
+
+                _dom.main.style.visibility = "hidden"
+                question.style.visibility = "visible"
+                question.style.top = "0px"
 
                 if(_dom.viewingQuestion == true) {
                     _dom.menu_button.style.cursor = "not-allowed"
                 }
 
-                question.setAttribute('class', 'questions animate__animated animate__zoomIn')
-                // console.log(question.attributes)
 
-                _dom.main.style.visibility = "hidden"
-                question.style.visibility = "visible"
+                choices.addEventListener('click', function(event) {
+                    let clicked_answer = event.target
+                    if(_dom.viewingQuestion == true && question_id) {
+                        if(clicked_answer.id.includes(question_id)) {
+                            _dom.correct_answer = true
+                            showQuestion()
+                           _dom.correct_answer = false
+                        }
+                    }
+                })
 
-                setTimeout(hide_question, 1)
-                setTimeout(show_question, _dom.questionLength)
+                setTimeout(playAnimation(), 1)
+                setTimeout(showQuestion, _dom.questionLength)
 
-                function show_question() {
+
+                function showQuestion() {
                     let question_number = clicked_element.classList[1]
-                    let question_val = clicked_element.classList[2]
+                    let question_value = clicked_element.classList[2]
                     
                     stopAnimation()
                     question.style.visibility = "hidden"
                     clicked_element.innerText = "-"
                     clicked_element.style.pointerEvents = "none"
-                    clicked_element.setAttribute('class', `boxes ${question_number} ${question_val} used`)
+                    clicked_element.setAttribute('class', `boxes ${question_number} ${question_value} used`)
                     _dom.viewingQuestion = false
                     _dom.menu_button.style.cursor = "pointer"
-                }
-
-                function hide_question() {
-                    playAnimation()
                 }
           }
         } catch(error) {
@@ -84,3 +98,7 @@ function stopMusic(sound) {
     sound.pause()
     sound.currentTime = 0
 }
+
+
+
+export{stopAnimation}
