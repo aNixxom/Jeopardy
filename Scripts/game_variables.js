ccf7d4904fecb665e4ea8db8acb969ede5bc85e3
aNixxom <jacob@jacobm.dev>
2023-01-12 10:55:04 -0500

    Removed unused variables

--- Scripts/game_variables.js
+++ Scripts/game_variables.js
@@ -31,7 +31,6 @@ const _dom = {
   editModeSwitch: document.querySelector(".settings-toggle-button-two"),
   doublePointsSwitch: document.querySelector(".settings-toggle-button-three"),
   questionRefresh: document.querySelector('.settings-toggle-button-four'),
-  doubleTimeCheatEnabled: false, // Default false
   editModeToggled: false, // Default false
   doublePointToggled: false, // Default flase
   isOpen: false, // Default false
@@ -41,55 +40,6 @@ const _dom = {
   current_theme: document.getElementById("current-theme"),
   // Questions --------------------
   viewingQuestion: false, // Default false
-  q1: document.getElementById("q1"),
-  q2: document.getElementById("q2"),
-  q3: document.getElementById("q3"),
-  q4: document.getElementById("q4"),
-  q5: document.getElementById("q5"),
-  q6: document.getElementById("q6"),
-  q7: document.getElementById("q7"),
-  q8: document.getElementById("q8"),
-  q9: document.getElementById("q9"),
-  q10: document.getElementById("q10"),
-  q11: document.getElementById("q11"),
-  q12: document.getElementById("q12"),
-  q13: document.getElementById("q13"),
-  q14: document.getElementById("q14"),
-  q15: document.getElementById("q15"),
-  q16: document.getElementById("q16"),
-  q17: document.getElementById("q17"),
-  q18: document.getElementById("q18"),
-  q19: document.getElementById("q19"),
-  q20: document.getElementById("q20"),
-  // Click Event objects --------- (First Row ($200))
-  r1c1: document.getElementById("r1c1"), //q1
-  r1c2: document.getElementById("r1c2"),
-  r1c3: document.getElementById("r1c3"),
-  r1c4: document.getElementById("r1c4"),
-  r1c1ra: document.getElementById('q1_correct'),
-  r1c2ra: document.getElementById('q2_correct'),
-  r1c3ra: document.getElementById('q3_correct'),
-  r1c4ra: document.getElementById('q4_correct'),
-  // Click Event objects --------- (Second Row ($400))
-  r2c1: document.getElementById("r2c1"), //q2
-  r2c2: document.getElementById("r2c2"),
-  r2c3: document.getElementById("r2c3"),
-  r2c4: document.getElementById("r2c4"),
-  // Click Evnet objects ---------- (Third Row ($600))
-  r3c1: document.getElementById("r3c1"), //q3
-  r3c2: document.getElementById("r3c2"),
-  r3c3: document.getElementById("r3c3"),
-  r3c4: document.getElementById("r3c4"),
-  // Click Evnet objects ---------- (Fourth Row ($800))
-  r4c1: document.getElementById("r4c1"), //q4
-  r4c2: document.getElementById("r4c2"),
-  r4c3: document.getElementById("r4c3"),
-  r4c4: document.getElementById("r4c4"),
-  // Click Evnet objects ---------- (Fith Row ($1000))
-  r5c1: document.getElementById("r5c1"), //q5
-  r5c2: document.getElementById("r5c2"),
-  r5c3: document.getElementById("r5c3"),
-  r5c4: document.getElementById("r5c4"),
   // Take Player Score
   take_p1: document.getElementById("take_p1"),
   take_p2: document.getElementById("take_p2"),
