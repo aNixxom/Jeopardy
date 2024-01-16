5782b2bc3de51d74add86fb10fada7aa9d31c6cf
aNixxom <91269723+aNixxom@users.noreply.github.com>
2024-01-16 10:56:07 -0500

    Update game_main.js

--- Scripts/game_main.js
+++ Scripts/game_main.js
@@ -1,13 +1,19 @@
 import {_dom} from '/Scripts/game_variables.js'
-import { systemMessage} from '/Scripts/createNewPlayers.js'
+import { systemMessage} from '/Scripts/createNewPlayers.js' 
 
 let cell
+
 const game_table = document.createElement('table')
 game_table.setAttribute('id', 'main_table')
 game_table.setAttribute('class', 'main_table')
 game_table.addEventListener('click', function(event) {
     let clicked_box = event.target
     if(event.target.hasAttribute('data-question')) {
+
+        let point_value = clicked_box.innerText.replace('$', '')
+        point_value = parseFloat(point_value)
+        
+        _dom.default_point_value = point_value
         _dom.viewingQuestion = true
         _dom.answered_question = false
 
@@ -29,15 +35,15 @@ game_table.addEventListener('click', function(event) {
         _dom.menu_button.style.cursor = "not-allowed"
 
         choices.addEventListener('click', function(event) {
-            let correctAnswer = document.getElementById(`${choices.id}-correct`).innerHTML
+            let correctAnswer = document.getElementById(`${clicked_question.id}-answer`).innerHTML
             let clicked_answer = event.target
-            if(clicked_answer.getAttribute('data-choices') === 'correct' && _dom.viewingQuestion === true) {
+            if(clicked_answer.getAttribute('data-choices') === 'answer' && _dom.viewingQuestion === true) {
                 systemMessage("Correct!")
                 _dom.answered_question = true
                 stopAudio(_dom.corret_answer_sound)
                 _dom.corret_answer_sound.play()
                 exitQuestion()
-            } else if(clicked_answer.getAttribute('data-choice') != 'correct') {
+            } else if(clicked_answer.getAttribute('data-choice') != 'answer') {
                 systemMessage(`The answer was: ${correctAnswer}`)
                 _dom.answered_question = true
                 stopAudio(_dom.times_up)
@@ -46,7 +52,7 @@ game_table.addEventListener('click', function(event) {
             }
         })
 
-        question_timer.addEventListener("animationend", function(event) {
+        question_timer.addEventListener("animationend", function() {
             if(_dom.answered_question == false) {
                 _dom.times_up.play()
                 exitQuestion()
@@ -68,12 +74,12 @@ game_table.addEventListener('click', function(event) {
 
 for (let i = 0; i < 5; i++) {
     let rows = game_table.insertRow(i)
-    rows.id = `${i}r${i}c`
+    rows.id = `${i + 1}r${i + 1}c`
     
     for(let y = 0; y < 5; y++) {
 
         cell = rows.insertCell(y)
-        cell.id = `r${i}c${y}`
+        cell.id = `r${i + 1}c${y + 1}`
         cell.innerText = cell.id 
         cell.setAttribute('class', 'boxes')
         cell.setAttribute('data-question', 'box')
@@ -83,9 +89,8 @@ for (let i = 0; i < 5; i++) {
         const choices = document.createElement('div')
         const timer_container = document.createElement('div')
         const timer_bar = document.createElement('div')
-        choices.id = `${cell.id}-ch`
 
-        for(let k = 0; k < 4; k++) {
+        for(let k = 0; k < 3; k++) {
             const choices_option = document.createElement('p')
             choices_option.innerHTML = `${k}`   
             choices.setAttribute('data-choice', 'choice')
@@ -108,42 +113,64 @@ for (let i = 0; i < 5; i++) {
 } _dom.main.insertBefore(game_table, document.getElementById('player-info-table'))
 
 let headers = game_table.insertRow(0)
-headers.setAttribute('class', 'headers-category')
-headers.id = "headers"
+headers.setAttribute('class', 'headers')
 for(let i = 0; i < 5; i++) {
     headers.insertCell()
-    headers.children[i].setAttribute('class', 'headers-category')
-    headers.children[i].setAttribute('class', 'headers')
 }
 
+let choices = document.querySelectorAll('[data-choice="choice"]')
+let cells = document.querySelectorAll('.boxes')
 let questions = document.querySelectorAll('.questions')
-let choices = document.querySelectorAll('.boxes')
 
 questions.forEach((element, index) => {
     element.id = `q${index + 1}`
 })
 
-//TODO: use for loop to make this run faster
+fetch('./gfcsmeeting.json')
+    .then((response) => response.json())
+    .then((info) => {
+        for(let i = 0; i < info['headings'].length; i++) {
+            headers.children[i].innerHTML = info['headings'][i]
+        }
 
+        cells.forEach((element, index) => {
+            element.childNodes[0].textContent = info['questions'][index].value
+            element.childNodes[1].children[0].innerHTML = info['questions'][index]['question']
+        })
+        choices.forEach((element, index) => {
+            getRandomOptionSlot(element, index, 'answer', info)
+            getRandomOptionSlot(element, index, 'wrong_1', info)
+            getRandomOptionSlot(element, index, 'wrong_2', info)
+        })
+    })
 
-choices.forEach((element, index) => {
-    if(element.id.includes('r0')) {
-        element.childNodes[0].textContent = '$200'
-    } else if(element.id.includes('r1')) {
-        element.childNodes[0].textContent = '$400'
-    } else if(element.id.includes('r2')) {
-        element.childNodes[0].textContent = '$600'
-    } else if(element.id.includes('r3')) {
-        element.childNodes[0].textContent = '$800'
-    } else if(element.id.includes('r4')) {
-        element.childNodes[0].textContent = '$1000'
+function getRandomOptionSlot(element, index, option, json) {
+    let pickedSlot = element.children[pickRadomElement(3)]
+    let pickedValidSlot = false
+    do {
+        if(pickedSlot.hasAttribute('data-choices', 'correct') || pickedSlot.hasAttribute('data-choices', 'wrong_1')) {
+            pickedSlot = element.children[pickRadomElement(3)]
+        } else {
+            pickedValidSlot = true
+            pickedSlot.innerHTML = json['questions'][index][`${option}`]
+            if(pickedSlot.innerHTML === "undefined") {
+                pickedSlot.remove()
+            }
+            pickedSlot.setAttribute('data-choices', `${option}`)
+            pickedSlot.setAttribute('id', `${element.parentElement.id}-${option}`)
+        }
     }
-})
+    while (pickedValidSlot == false)
+}
 
-
+function pickRadomElement(max) {
+    return Math.floor(Math.random() * max)
+}
 
 function stopAudio(audio) {
     audio.pause()
     audio.currentTime = 0
 }
+
+
 _dom.question_length_icon.innerHTML = ` ${_dom.questionLength / 1000}s `
